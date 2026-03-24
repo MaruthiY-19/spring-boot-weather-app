3786b20c80fe1ff2ebb611845a7b90737da3db76
MaruthiY-19 <ymaruthi99@gmail.com>
2026-03-24 12:02:31 +0530

    Update Main.java

--- src/main/java/com/weatherapp/weather_app_backend/model/Main.java
+++ src/main/java/com/weatherapp/weather_app_backend/model/Main.java
@@ -1,14 +1,11 @@
 package com.weatherapp.weather_app_backend.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-// ... other imports if needed
+
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Main {
     private double temp;
     private int humidity;
-    // ... other fields like feels_like, temp_min, temp_max, pressure
-
-    // Constructor (if any)
-    // Getters and Setters for all fields
+  
 }
